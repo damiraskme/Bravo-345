6691f86fd203b457f30bc4f9ebcdacae09204300
ChanaChanaboon <157045459+ChanaChanaboon@users.noreply.github.com>
2024-04-25 14:41:39 -0400

    Create AccessViolationException.java

---
+++ doc/methods/AccessViolationException.java
@@ -0,0 +1,6 @@
+ // AccessViolationException.java
+ public class AccessViolationException extends Exception {
+    public AccessViolationException(String message) {
+        super(message);
+    }
+}
